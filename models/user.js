7788babf8bed3b26c191e375c7fd1ec2a200e80e
menth0s <marslite@pm.me>
2025-05-20 03:59:25 +0200

    Implemented articleSchema

--- models/user.js
+++ models/user.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
     unique: true,
     validate: {
       validator(value) {
-        return validator.isURL(value);
+        return validator.isEmail(value);
       },
       message: "You must use a valid email",
     },
