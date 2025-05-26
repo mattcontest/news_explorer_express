892c651ea9ee8df26b3032d996316c6a3df53af6
menth0s <marslite@pm.me>
2025-05-26 12:14:31 +0200

    Added like and dislike features to the controller

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require("mongoose");
 const Article = require("../models/article");
 
 const getArticles = (req, res, next) => {
@@ -57,4 +58,55 @@ const deleteArticle = (req, res, next) => {
     .catch((err) => console.error(err));
 };
 
+const likeItem = (req, res, next) => {
+  const { itemId } = req.params;
+  Article.findByIdAndUpdate(
+    itemId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true }
+  )
+    .orFail(() => {
+      return res.status(404).send({ message: "Not exisisting id in the db" });
+    })
+    .populate("owner")
+    .then((updatedItem) => res.status(200).json(updatedItem))
+    .catch((err) => {
+      console.log("Check error", err);
+      if (err.statusCode == 404) {
+        return res.status(404).send({ message: "Not found in the db" });
+      }
+    });
+};
+
+const dislikeItem = (req, res, next) => {
+  const { itemId } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(itemId)) {
+    return res.status(400).send({ message: "Bad Request: Invalid Format" });
+  }
+
+  return Article.findByIdAndUpdate(
+    itemId,
+    { $pull: { likes: req.user._id } },
+    { new: true }
+  )
+    .populate("owner")
+    .orFail()
+    .then((updatedItem) => res.status(200).json(updatedItem))
+    .catch((err) => {
+      if (err.name === "DocumentNotFoundError") {
+        return res
+          .status(404)
+          .send({ message: "Article to dislike not found" });
+      }
+
+      if (err.name === "CastError") {
+        return res
+          .status(400)
+          .send({ message: "Bad Request: Cast Error when disliking an item" });
+      }
+      return res
+        .status(500)
+        .send({ message: "500 Server Error when deleting an  Item" });
+    });
+};
 module.exports = { getArticles, createArticle, deleteArticle };
