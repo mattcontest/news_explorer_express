a5c0313cbc43dc6f81664a6ba6e6c31a37222443
menth0s <marslite@pm.me>
2025-06-19 19:31:45 +0200

    Updating express limiter

--- middlewares/express-limiter.js
+++ middlewares/express-limiter.js
@@ -3,7 +3,7 @@ const { rateLimit } = require("express-rate-limit");
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, //~15 Mins
   limit: 100, //Limit each ip to 100 request per window
-  standardHeaders: "draft-h",
+  // standardHeaders: "draft-h",
   legacyHeaders: false,
 });
 
