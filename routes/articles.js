83c7b05a99b9ae00495acd734ae0687d300aac97
menth0s <marslite@pm.me>
2025-07-08 00:42:06 +0200

    Implementing validating article

--- routes/articles.js
+++ routes/articles.js
@@ -6,11 +6,12 @@ const {
   createArticle,
 } = require("../controllers/articles");
 const auth = require("../middlewares/auth");
+const { validateArticle } = require("../middlewares/validation");
 
 router.use(auth);
 router.get("/", getArticles);
 router.delete("/:articleId", deleteArticle);
-router.put("/:articleId/likes", likeArticle);
+router.put("/:articleId/likes", validateArticle, likeArticle);
 //  But instead this?
 router.post("/", createArticle);
 // router.delete("/:articleId/likes", dislikeArticle);
