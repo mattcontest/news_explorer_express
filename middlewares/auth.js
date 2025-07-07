6c37578cc054579d78da45ca67cc566bcbc75362
menth0s <marslite@pm.me>
2025-07-08 01:18:39 +0200

    Handled centralized error inside auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,14 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
+const UnauthorizedError = require("../errors/unauthorizedError");
 
 module.exports = (req, res, next) => {
   console.log("🛡️ AUTH MIDDLEWARE TRIGGERED");
 
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith("Bearer")) {
-    return res.status(401).send({ message: "Authorization required!" });
+    // return res.status(401).send({ message: "Authorization required!" });
+    return next(new UnauthorizedError("Authorization required!"));
   }
 
   const token = authorization.replace("Bearer ", "");
@@ -18,9 +20,14 @@ module.exports = (req, res, next) => {
     console.log("PAYLOAD", token, JWT_SECRET);
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    return res.status(401).send({
-      message: "Authorization required ~ Didn't pass jwt verification",
-    });
+    // return res.status(401).send({
+    //   message: "Authorization required ~ Didn't pass jwt verification",
+    // });
+    return next(
+      new UnauthorizedError(
+        "Authorization required ~ Didn't pass jwt verification"
+      )
+    );
   }
 
   req.user = payload;
